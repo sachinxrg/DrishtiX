215de63afdf19ca64e89b5c1f3cd3868bfbcd60c
Sachidanand Gond <srkashyap126@outlook.com>
2026-08-02 16:14:55 +0530

    chore: remove leaked secret scripts and update gitignore

--- services/drishtix-app/enable_fbi.js
+++
@@ -1,2 +0,0 @@
-db.alert_config.updateOne({config_key: 'ingestion_enabled'}, {$set: {config_value: 'true'}}, {upsert: true});
-db.alert_config.updateOne({config_key: 'fbi_api_key'}, {$set: {config_value: 'YOUR_API_KEY_HERE'}}, {upsert: true});
--- services/drishtix-app/update_fbi_key.js
+++
@@ -1,1 +0,0 @@
-db.alert_config.updateOne({config_key: 'fbi_api_key'}, {$set: {config_value: 'Q7IdKXdJ4bb9etLQobHooMcrm4qooSU6Y9hj0XDY'}}, {upsert: true});
